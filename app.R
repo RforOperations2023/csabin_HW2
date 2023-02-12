8fa2795e86597c4cb5e6a86e9e3024bdcea81e05
c-sabin <97855633+c-sabin@users.noreply.github.com>
2023-02-11 21:04:32 -0500

    create structure for shiny app code

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 ### Caroline Sabin
 ### 94-880 Section A3
 ### R Shiny for Operations Management
-### HW 2: 
+### HW 2: Create a Dashboard 
 
 #### PROJECT SET-UP ####
 
@@ -21,3 +21,58 @@ ghg_inventory <- read.csv("Greenhouse_Gas_Inventory.csv")
 
 
 #### DATA MANIPULATION ####
+
+# Change column names
+colnames(ghg_inventory) <- c("inventory", "sector", "source", "year", 
+                             "consumption_units","consumption", "emissions", "ID")
+
+# Change to df
+ghg_inventory <- data.frame(ghg_inventory)
+
+# Change variables to factors
+ghg_inventory$inventory <- as.factor(ghg_inventory$inventory)
+ghg_inventory$sector <- as.factor(ghg_inventory$sector)
+ghg_inventory$year <- as.factor(ghg_inventory$year)
+
+
+
+
+
+
+##### SHINY APP CHUNKS #####
+
+# User Interface
+
+ui <- fluidPage(theme = shinytheme("cosmo"),
+                
+    # Title
+    titlePanel("Washington D.C. Citywide Greenhouse Gas Emissions: 2006-2020")
+                
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+                    
+            # Inputs
+        ),
+                  
+    # Main Panel
+        mainPanel(
+            # Outputs
+                  )
+    )
+)
+
+
+
+
+# Server
+
+server <- function(input, output) {}
+    
+    
+
+
+# Call ShinyApp
+    
+shinyApp(ui = ui, server = server)
+
